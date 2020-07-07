f7a18a84ca6490ac4ab3e1a9267bca73aa4bc8eb
cooperoperator <george@tactics.coop>
2020-07-07 10:29:10 -0400

    Add bare-bones customer session, show appropriate interconnection requests, remove customerLanding2 mock-up.

--- r3it/web.py
+++ r3it/web.py
@@ -2,6 +2,9 @@ import copy
 import csv
 import json
 import time
+import flask_login
+import flask_wtf
+import os, hashlib, random
 from random import choice as pick
 
 from flask import Flask, redirect, request, send_from_directory
@@ -10,14 +13,61 @@ from jinja2 import Template
 import interconnection
 
 app = Flask(__name__)
+r3itDir = os.path.dirname(os.path.abspath(__file__))
+
+users = {'george@cooperativetactics.com': {'password': 'secret', 'type': 'customer'}}
+
+def cryptoRandomString():
+    ''' Generate a cryptographically secure random string for signing/encrypting cookies. '''
+    if 'COOKIE_KEY' in globals():
+        return COOKIE_KEY
+    else:
+        return hashlib.md5(str(random.random()).encode('utf-8') + str(time.time()).encode('utf-8')).hexdigest()
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+app.secret_key = cryptoRandomString()
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def load_user(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+    user = User()
+    user.id = email
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+
+@app.route('/login/engineer', methods=['GET', 'POST'])
+def login_engineer():
+    template = Template(open('templates/loginEngineer.html', 'r').read())
+    return template.render()
+
+
+@app.route('/login/customer', methods=['GET', 'POST'])
+def login_customer():
+    template = Template(open('templates/loginCustomer.html', 'r').read())
+    return template.render()
+
 
 statuses = ['Approved', 'Pending', 'Customer Options Meeting Proposed', 'Customer Options Meeting Scheduled', 'Interconnection Agreement Profferred', 'Interconnection Agreement Executed', 'Permission to Operate Profferred', 'Permission to Operate Executed', 'In Operation', 'Out of Service']
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def hello_world(): return 'Hello, World!'
 
 @app.route('/queue')
 def queue():
@@ -31,19 +81,6 @@ def queue():
     template = Template(open('templates/queue.html', 'r').read())
     return template.render(data=queue_data)
 
-
-@app.route('/login/engineer')
-def login_engineer():
-    template = Template(open('templates/loginEngineer.html', 'r').read())
-    return template.render()
-
-
-@app.route('/login/customer')
-def login_customer():
-    template = Template(open('templates/loginCustomer.html', 'r').read())
-    return template.render()
-
-
 @app.route('/overview')
 def overview():
     template = Template(open('templates/overview.html', 'r').read())
@@ -51,8 +88,31 @@ def overview():
 
 @app.route('/customerLanding')
 def customerLanding():
+    queue_data = []
+    with open('data/queue.json') as queue:
+        data = json.load(queue)
+        for id, value in data.items():
+            if value["Email (Customer)"] == flask_login.current_user.id:
+                datarow = [id, value['Time of Request'],
+                       value['Address (Facility)'], value['Status']]
+                queue_data.append(datarow)
+                
+
     template = Template(open('templates/customerLanding.html', 'r').read())
-    return template.render()
+    return template.render(data=queue_data)
+
+@app.route('/login')
+def login():
+    user = load_user(request.args['username'])
+    flask_login.login_user(user)
+    nextUrl = request.args['next']
+    return redirect(nextUrl)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/login/{}'.format(users[flask_login.current_user.id]['type']))
+
 
 @app.route('/customerLanding2')
 def customerLanding2():
@@ -69,11 +129,6 @@ def thankyou():
     template = Template(open('templates/applicationSubmitted.html', 'r').read())
     return template.render()
 
-@app.route('/customerLanding')
-def customer_landing():
-    template = Template(open('templates/customerLanding.html', 'r').read())
-    return template.render()
-
 
 @app.route('/report/<id>')
 def report(id):
@@ -88,7 +143,6 @@ def report(id):
 
 @app.route('/add-to-queue', methods=['GET', 'POST'])
 def add_to_queue():
-    print(request.form)
     status = interconnection.compute_status(request.form)
     interconnection_request = {}
     for key, value in request.form.items():
@@ -110,6 +164,7 @@ def send_file(fullPath):
 
 
 @app.route('/application')
+@flask_login.login_required
 def application():
     firstNames = ['James', 'John', 'Robert', 'Michael', 'William', 'David', 'Richard', 'Charles', 'Mary', 'Patricia', 'Linda', 'Barbara', 'Elizabeth', 'Jennifer', 'Maria', 'Susan', 'Margaret']
     lastNames = ['Smith', 'Jones', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis']
@@ -134,7 +189,7 @@ def application():
     'phone' : '({}{}) {}{} - {}'.format(pick(range(2,9)), pick(range(10,99)), pick(range(2,9)), pick(range(10,99)), pick(range(1000,9999))),
     'size' : '{}'.format(pick(sizes)),
     'voltage' : '{}'.format(pick(voltages)),
-    'email' : '{}.{}@gmail.com'.format(firstname, lastname)
+    'email' : str(flask_login.current_user.id)
     }
     
     template = Template(open('templates/application.html', 'r').read())
