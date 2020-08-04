f2ca57cc7d35c36edce2d0c4639808de8e8cfb9e
David Pinney <david@pinney.org>
2020-08-04 13:13:01 -0400

    interconn spec update

--- r3it/interconnection.py
+++ r3it/interconnection.py
@@ -6,18 +6,40 @@ from omf.models import derInterconnection
 FEEDER_PATH1 = '/static/feeder1.omd'
 
 '''
+DIRECTORY STRUCTURE
 /data/queue/
-	after_request_1
-	after_request_2
+	after_request_1/
+        <all derInterconnection files>
+        <all associated request inputs>
+	after_request_2/
+        ...
 	...
-	after_request_n
+	after_request_n/
 
-def build_model(id):
+FUNCTIONS
+def push_model(new_request_data_dict):
 	current_omd = f'/data/queue/after_request_{id - 1}'
 	derInterconnection.new('/data/queue/after_request_{id}, {'omd_path':'/static/base_circuit.omd'})
+    if queue_blocked_by_fail:
+        pass # do something
+    else:
+        pass # do something else
 
-def withdrawal(id):
-	pass #todo later: go through and rebuild the queue starting at id - 1.
+def get_frontend_data():
+    for req in os.listdir('/data/queue'):
+        pass #pull required data
+
+def upgrade_to_mitigate_fail(id, upgrade_info_dict):
+    pass # do stuff for base model
+    for req in os.listdir('/data/queue'):
+        pass # run with new mitigated data.
+
+def _test(number_to_push, random_inputs_or_not):
+    # Three major cases:
+    # pass, pass
+    # fail, pend
+    # pass, fail
+    pass
 '''
 
 def compute_status(interconnection_form):
