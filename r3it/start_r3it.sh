0a3aef734e2c8a12479a5badffda4f0a633894f0
George <george@Georges-MacBook.local>
2020-12-04 16:52:33 +0000

    Add port 80 redirect (derived from OMF webprod)

--- r3it/start_r3it.sh
+++ r3it/start_r3it.sh
@@ -1,1 +1,25 @@
-gunicorn 'wsgi:app'
+import os, web, logging
+from subprocess import Popen
+
+# Note: sudo python start_r3it.py on macOS since this will open low numbered ports.
+# If you need some test certs: openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout privkey.pem -days 365 -subj '/CN=localhost/O=NoCompany/C=US'
+
+reApp = web.Flask('R3IT')
+
+@reApp.route('/')
+def index():
+	return 'NA'
+
+@reApp.before_request
+def before_request():
+	if web.request.url.startswith('http://'):
+		url = web.request.url.replace('http://', 'https://', 1)
+		code = 301
+		return web.redirect(url, code=code)
+
+if __name__ == "__main__":
+	# Start redirector:
+	redirProc = Popen(['gunicorn', '-w', '4', '-b', '0.0.0.0:80', 'webProd:reApp'])
+	# Start application:
+	appProc = Popen(['gunicorn', '-w', '4', '-b', '0.0.0.0:443', '--certfile=cert.pem', '--ca-certs=fullchain.pem', '--keyfile=privkey.pem', '--preload', 'web:app','--worker-class=sync', '--access-logfile', 'omf.access.log', '--error-logfile', 'omf.error.log', '--capture-output'])
+	appProc.wait()
