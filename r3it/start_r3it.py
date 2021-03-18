07fef9e5124c2968a9c8a9d433b8411349161920
George Walker <georgewalkeriv@gmail.com>
2021-03-18 14:34:33 +0000

    Move redirector to Flask from webpy.

--- r3it/start_r3it.py
+++ r3it/start_r3it.py
@@ -1,12 +1,11 @@
-import os, web, logging, config
-
-from flask.helpers import send_from_directory
+import os, logging, config
+from flask import Flask, redirect, request, render_template, url_for, send_from_directory
 from subprocess import Popen
 
 # Note: sudo python start_r3it.py on macOS since this will open low numbered ports.
 # If you need some test certs: openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout privkey.pem -days 365 -subj '/CN=localhost/O=NoCompany/C=US'
 
-reApp = web.Flask('R3ITR')
+reApp = Flask('R3ITR')
 
 @reApp.route('/')
 def index():
@@ -26,7 +25,7 @@ def before_request():
 
 if __name__ == "__main__":
 	# Start redirector:
-	redirProc = Popen(['gunicorn', '-w', '4', '-b', '0.0.0.0:80', 'start_r3it:reApp'])
+	redirProc = Popen(['gunicorn', '-w', '1', '-b', '0.0.0.0:80', 'start_r3it:reApp'])
 	# Start application:
 	appProc = Popen(['gunicorn', '-w', '4', '-b', '0.0.0.0:443', '--certfile=cert.pem', '--ca-certs=fullchain.pem', '--keyfile=privkey.pem', '--preload', 'web:app','--worker-class=sync', '--access-logfile', 'r3it.access.log', '--error-logfile', 'r3it.error.log', '--capture-output'])
 	appProc.wait()
