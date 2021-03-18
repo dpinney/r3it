32a2100f2f59599fd8e04f4e8b985020f419104a
George Walker <georgewalkeriv@gmail.com>
2021-03-18 11:04:54 +0000

    Missing paren.

--- r3it/web.py
+++ r3it/web.py
@@ -270,7 +270,7 @@ def download(id, doc, filename):
 @flask_login.login_required
 def cert_renewal(filename):
     log('Attempting to renew TLS certificate')
-    return send_from_directory(os.path.join(app.root_path,'.well-known', filename)
+    return send_from_directory(os.path.join(app.root_path,'.well-known', filename))
     
 @app.route('/add-to-queue', methods=['GET', 'POST'])
 @flask_login.login_required
