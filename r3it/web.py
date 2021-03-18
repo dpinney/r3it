2d2579496a821c7852733b453017df2b0cc9f951
George Walker <georgewalkeriv@gmail.com>
2021-03-18 11:01:35 +0000

    Grant Let's Encrypt access to .well-known dir.

--- r3it/web.py
+++ r3it/web.py
@@ -266,6 +266,12 @@ def download(id, doc, filename):
         log('Download not initiated; user not authorized to view document')
         return redirect('/')
 
+@app.route('/.well-known/<path:filename>')
+@flask_login.login_required
+def cert_renewal(filename):
+    log('Attempting to renew TLS certificate')
+    return send_from_directory(os.path.join(app.root_path,'.well-known', filename)
+    
 @app.route('/add-to-queue', methods=['GET', 'POST'])
 @flask_login.login_required
 def add_to_appQueue():
