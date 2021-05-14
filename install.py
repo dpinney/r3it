f79a8a150556deae5b4fb89e462da1430dad04bb
George Walker <georgewalkeriv@gmail.com>
2021-05-14 12:57:14 +0100

    Remove non-working bash magic.

--- install.py
+++ install.py
@@ -26,7 +26,8 @@ os.system("sudo ln -s /etc/letsencrypt/live/" + config.DOMAIN + "/chain.pem /opt
 
 # install systemd unit files for r3it and certificate renewal.
 os.system("sudo ln -s /opt/r3it/r3it/r3it.service /etc/systemd/system/r3it.service")
-os.system("sudo ln -s /opt/r3it/r3it/cert.{s..t}* /etc/systemd/system/")
+os.system("sudo ln -s /opt/r3it/r3it/cert.service /etc/systemd/system/")
+os.system("sudo ln -s /opt/r3it/r3it/cert.timer /etc/systemd/system/")
 
 # create log file
 os.system("sudo mkdir /opt/r3it/r3it/data")
@@ -40,9 +41,12 @@ os.system("sudo useradd -r r3it")
 os.system("sudo chown -R r3it:r3it /opt/r3it")
 
 # configure authbind so r3it can bind to low-numbered ports sans root.
-os.system("sudo touch /etc/authbind/byport/{80,443}")
-os.system("sudo chown r3it:r3it /etc/authbind/byport/{80,443}")
-os.system("sudo chmod 710 /etc/authbind/byport/{80,443}")
+os.system("sudo touch /etc/authbind/byport/80")
+os.system("sudo touch /etc/authbind/byport/443")
+os.system("sudo chown r3it:r3it /etc/authbind/byport/80")
+os.system("sudo chown r3it:r3it /etc/authbind/byport/443")
+os.system("sudo chmod 710 /etc/authbind/byport/80")
+os.system("sudo chmod 710 /etc/authbind/byport/443")
 
 # enable r3it
 os.system("sudo systemctl enable /etc/systemd/system/r3it.service")
